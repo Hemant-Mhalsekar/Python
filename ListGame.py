9902b6388505367efa3c3f7d779305ca0251d4ac
ItsMeHemant <hemantmhalsekar01@gmail.com>
2023-09-07 23:14:11 +0530

    Created a LIst Game

---
+++ ListGame.py
@@ -0,0 +1,54 @@
+# A game based on strings where user input is taken and displayed back 
+
+game_list =[0,1,2]
+
+def display_game(game_list):
+    print("Here is the current list: ")
+    print(game_list)
+
+
+def position_choice():
+    choice ="Wrong"
+
+    while choice not in ['0','1','2']:
+        choice = input ("Pick a position (0,1,2): ")
+
+        if choice not in ['0', '1', '2']:
+            print("Sorry, invalid choice !")
+
+        return int(choice)
+
+def replacement_choice(game_list, position):
+    user_placement = input("Type the string to place at position: ")
+
+    game_list[position] = user_placement
+
+    return game_list
+
+def gameon_choice():
+     choice ="Wrong"
+
+     while choice not in ['Y','N']:
+        choice = input ("Keep playing (Y, N): ")
+
+        if choice not in ['Y','N']:
+            print("Sorry, invalid choice !")
+
+        if choice =='Y':
+            return True
+        else:
+            return False
+        
+game_on = True
+game_list = [0,1,2]
+
+while game_on:
+    display_game(game_list)
+
+    position = position_choice()
+
+    game_list = replacement_choice(game_list, position)
+
+    display_game(game_list)
+
+    game_on = gameon_choice()
