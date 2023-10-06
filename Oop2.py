0b700dda64b485f947643ab0c24dfdbb534680b0
ItsMeHemant <hemantmhalsekar01@gmail.com>
2023-10-06 23:27:15 +0530

    This is a modified version of oop1.py

---
+++ Oop2.py
@@ -0,0 +1,12 @@
+#Object Oriented Programming example-2
+class Dog():
+    def __init__(self, breed, name, spots):
+        self.breed = breed
+        self.name = name
+        self.spots = spots
+
+my_dog = Dog(breed='Lab', name="Sammy", spots=True)
+
+print(my_dog.breed)
+print(my_dog.name)
+print(my_dog.spots)
