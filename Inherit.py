2c7fc76ef8c6998b242c1c0541dde4b13f3d78ba
ItsMeHemant <hemantmhalsekar01@gmail.com>
2023-10-07 13:57:18 +0530

    Created derived class

--- Inherit.py
+++ Inherit.py
@@ -11,3 +11,18 @@ class Animal():
     
     def eat(self):
         print("I am eating")
+
+#Derived class 
+
+class Dog(Animal):
+
+    def __init__(self):
+        Animal.__init__(self)
+        print("Dog created")
+
+    #Overriding the function
+    def who_am_i(self):
+        print("I am a dog")
+
+    def bark(self):
+        print("WOOF!")
