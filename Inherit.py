463f93d16a10d58dfed098f1f22129fecca9c2f5
ItsMeHemant <hemantmhalsekar01@gmail.com>
2023-10-07 13:53:09 +0530

    Created the parent class

--- Inherit.py
+++ Inherit.py
@@ -1,3 +1,13 @@
 #Inheretance Example
 #Base class (or prent class)
- 
+
+class Animal():
+
+    def __init__(self):
+        print("Animal created")
+
+    def who_am_i(self):
+        print("I am na animal")
+    
+    def eat(self):
+        print("I am eating")
