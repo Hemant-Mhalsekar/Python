44554edca8ae5338c27f44a0b1f0be824c1798dc
Hemant-Mhalsekar <hemantmhalsekar01@gmail.com>
2024-07-15 23:17:12 +0530

    Added all the questions

--- Quiz.py
+++ Quiz.py
@@ -14,3 +14,25 @@ if answer == "central processing unit":
 else:
     print("Wrong answer. The correct answer is central processing unit.")
 
+# Question 2
+answer = input("What does GPU stand for? ")
+if answer == "graphics processing unit":
+    print("Correct answer")
+else:
+    print("Wrong answer. The correct answer is graphics processing unit.")
+    
+
+# Question 3
+answer = input("What does RAM stand for? ")
+if answer == "random access memory":
+    print("Correct answer")
+else:
+    print("Wrong answer. The correct answer is random access memory.")
+    
+    
+# Question 4
+answer = input("What does PSU stand for? ")
+if answer == "power supply unit":
+    print("Correct answer")
+else:
+    print("Wrong answer. The correct answer is power supply unit.")
