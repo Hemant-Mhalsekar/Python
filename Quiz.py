8884fc031013a58a97888ba741734211edccf7c1
Hemant-Mhalsekar <hemantmhalsekar01@gmail.com>
2024-07-15 23:06:08 +0530

    added a first question.

--- Quiz.py
+++ Quiz.py
@@ -2,3 +2,10 @@ print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play the game? (yes/no): ")
 
+if playing != "yes":
+    quit()
+
+print("Let's begin!")
+
+# Question 1
+answer = input("What does CPU stand ")
